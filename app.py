d9dbf5e41bc7ee1dceb4583cff0437e7d3a544d1
David Bieber <dbieber@princeton.edu>
2012-01-12 14:36:42 -0500

    multiple droplets
    typo

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import os
+import json
 from flask import Flask
 from client import f
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+    return "ELE 201 project on Fountain Codes by David Bieber."
 
 @app.route("/droplet")
 def droplet():
     return f.droplet().toString()
+	
+@app.route("/droplet/<amt>")
+def droplets(amt):
+    out = []
+	for x in xrange(amt):
+		out.append(f.droplet().toString())
+	return json.dumps(out)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
